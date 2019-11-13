d777d3f274fbd8f4cf3b300ecf420a739fd528bd
Jacob Kleiner <jacobkleiner89@gmail.com>
2019-11-13 03:25:44 +0000

    Geocode Google API

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, length: {minimum: 4}, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
   geocoded_by :address
   after_validation :geocode
 end
