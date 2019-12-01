8034f38bb8394692dde24c0bd75398fc5edeedbd
Jacob Kleiner <jacobkleiner89@gmail.com>
2019-12-01 19:38:46 +0000

    Places_controller Fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,7 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-def index
-  @places = Place.all
-  end
+
 
 def new
   @place = Place.new
